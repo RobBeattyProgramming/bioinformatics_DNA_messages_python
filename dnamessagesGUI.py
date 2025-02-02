4aaa59729739bea04198b0057acc6c286e66adda
robbeattyprogramming <robbeattyprogramming@gmail.com>
2025-02-02 01:06:47 -0500

    added if statement for option argument in recallInfo function that recalls previous user input

--- dnamessagesGUI.py
+++ dnamessagesGUI.py
@@ -1,16 +1,18 @@
 # create gui
 # store previous info in a csv file
-
-
 import csv
+
 def recallInfo(option):
     with open('savedData.csv', mode ='r')as file:
         csvFile = csv.reader(file)
-        #for lines in csvFile:
-           #print(lines)
         
         #call at open of program
         if option == "previous":
-            pass
+            for lines in csvFile:
+                if "previous" == lines[0]:        
+                    return lines[1]
+                
         elif option == "savedData":
             pass
+
+print(recallInfo("previous"))
